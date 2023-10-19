9e3e7db31d9be93ae4d9a642fd86f67f9e9d2495
W1ncent <wincent.westerback@gritlab.ax>
2023-10-19 11:38:36 +0300

    update script

--- run_filler.sh
+++ run_filler.sh
@@ -7,14 +7,14 @@ git clone https://github.com/bomanviktor/filler.git
 cd filler
 
 # Ensure that build_and_run.sh is executable
-chmod +x build_and_run.sh
+chmod +x build.sh
 
 # Build and run the container, keeping it alive with an endless loop
-docker build -t filler .
-docker run --name filler_container -v "$(pwd)/solution":/filler/solution -d filler tail -f /dev/null
+#docker build -t filler .
+#docker run --name filler_container -v "$(pwd)/solution":/filler/solution -d filler tail -f /dev/null
 
 # Execute the compile script within the running container
-docker exec -it filler_container ./compile.sh
+#docker exec -it filler_container ./compile.sh
 
 # Attach to the container interactively
-docker exec -it filler_container /bin/bash
+#docker exec -it filler_container /bin/bash
