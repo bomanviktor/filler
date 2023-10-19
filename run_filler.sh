5515ba72ea7f82fc6d77c9c28429887215211d4b
W1ncent <wincent.westerback@gritlab.ax>
2023-10-19 09:28:10 +0300

    script for audit

---
+++ run_filler.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clone the repository from GitHub
+git clone https://github.com/bomanviktor/filler.git
+
+# Navigate into the cloned repository directory
+cd filler
+
+# Ensure that build_and_run.sh is executable
+chmod +x build_and_run.sh
+
+# Build and run the container, keeping it alive with an endless loop
+docker build -t filler .
+docker run --name filler_container -v "$(pwd)/solution":/filler/solution -d filler tail -f /dev/null
+
+# Execute the compile script within the running container
+docker exec -it filler_container ./compile.sh
+
+# Attach to the container interactively
+docker exec -it filler_container /bin/bash
